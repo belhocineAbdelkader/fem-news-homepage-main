ad5a9770655631a9832e76541e3dd6eaedb8414e
Blhocine Abdelkader <belhocine.abdlekadere@gmail.com>
2024-02-29 04:37:52 +0100

    init and start up  the project

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        project: "./tsconfig.json",
+    },
+    plugins: ["@typescript-eslint"],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended-type-checked",
+        "plugin:@typescript-eslint/stylistic-type-checked",
+        "plugin:prettier/recommended",
+    ],
+    ignorePatterns: [".eslintrc.cjs", "vite.config.ts"],
+    overrides: [
+        {
+            extends: ["plugin:@typescript-eslint/disable-type-checked"],
+            files: ["./vite.config.js"],
+        },
+    ],
+    rules: {},
+};
---
+++ vite.config.js
@@ -0,0 +1,52 @@
+// @ts-expect-error: This is a temporary workaround for issue XYZ
+/* eslint-disable prettier/prettier */
+// Import necessary modules from Vite and Node.js
+import { defineConfig } from "vite";
+import path from "node:path";
+
+// Vite configuration object
+export default defineConfig({
+    // Server configuration
+    server: {
+        host: true,
+        port: 8080,
+        open: true,
+        https: false,
+        // Proxy configuration for API requests
+        proxy: {
+            "/api": {
+                target: "https://jsonplaceholder.typicode.com",
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+        },
+    },
+
+    // Build configuration
+    build: {
+        // Asset output directory and minification options
+        assetsDir: "assets",
+        minify: "terser", // or 'esbuild' for esbuild-based minification
+        sourcemap: true,
+
+        // Rollup options for asset output file names and sourcemap file names
+        rollupOptions: {
+            output: {
+                assetFileNames: ({ name }) => {
+                    const ext = path.extname(name);
+                    // Organize assets based on file type
+                    if (ext === ".css") {
+                        return `assets/css/${name}`;
+                    } else if (ext === ".woff" || ext === ".woff2") {
+                        return `assets/fonts/${name}`;
+                    } else if (ext === ".js") {
+                        return `assets/js/${name}`;
+                    }
+                    // For other file types, use the default output format with hash
+                    return `assets/${name}`;
+                },
+                sourcemapFileNames: "assets/js/[name].[hash].js.map",
+            },
+        },
+    },
+});
